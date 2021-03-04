0ad9bba404f60f9e0540258fa1ff488226a196c7
Santhosh-Bosku <santhoshbablu1997@gmail.com>
2021-03-04 16:56:28 +0530

    r

--- alpha.js
+++ alpha.js
@@ -1,14 +1,15 @@
-window.onscroll = function() {scrollFunction()};
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
 
-function scrollFunction() {
-if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 ) 
-{
-document.getElementsById("bar-one").style.backgroundColor = "rgba(255, 255, 255)";
-document.getElementsById("bar-two").style.backgroundColor = "rgba(255, 255, 255)";
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
 }
-else 
-{
-document.getElementsById("bar-one").style.backgroundColor = "rgba(0, 0, 0)";
-document.getElementsById("bar-two").style.backgroundColor = "rgba(0, 0, 0)";
-}
-}
+
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
